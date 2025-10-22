07ca6dabab4fa4f7736d3dde1e78b4d128b9e275
vmfrcosta <vmfrcosta@gmail.com>
2025-10-22 17:52:22 -0300

    removed unecessary presence validation for ApiKey.shareable

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -2,7 +2,7 @@ class ApiKey < ApplicationRecord
   belongs_to :user
 
   validates :secret, presence: true
-  validates :shareable, presence: true, uniqueness: { scope: :user }
+  validates :shareable, uniqueness: { scope: :user }
 
   def self.generate!(user:, shareable: true)
     create!(user: user, secret: SecureRandom.hex(32), shareable: shareable)
